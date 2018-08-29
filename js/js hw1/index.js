3712cbcb52d597c90f63f155d0b7c18e822f17af
yuraostapenko <41306905+yuraostapenko@users.noreply.github.com>
2018-08-29 21:50:18 +0300

    added exchanges

--- js/js hw1/index.js
+++ js/js hw1/index.js
@@ -5,22 +5,23 @@ let wellcome = 'Wellcome!';
 let AccessIsDenied = 'Access is denied!';
 let canceledByUser = 'Canceled by user!';
 
-let login = prompt('enter your login').toLowerCase();;
-if (login !== null) {
-   if (login === adminLogin) {
-       let password = prompt('enter your password').toLowerCase();;
-       if (password !== null) {
+let login = prompt('enter your login');
+if (login === null) {
+    alert(canceledByUser);
+} else {
+    if (login === adminLogin) {
+        let password = prompt('enter your password');
+        if (password !== null) {
             if (password === adminPassword) {
                 alert(wellcome);
             } else {
                 alert(AccessIsDenied);
             }
-       } else {
-        alert(canceledByUser);
-       }
-   } else {
-    alert(AccessIsDenied);
-   }
-} else {
-    alert(canceledByUser);
+        } else {
+            alert(canceledByUser);
+        }
+    } else {
+        alert(AccessIsDenied);
+    }
 }
+
